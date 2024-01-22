7814b57dd20fcfb2a0a5e65a1cbb62917fcb098f
Amir-Subaih <86726698+Amir-Subaih@users.noreply.github.com>
2024-01-22 19:05:48 +0200

    comment

--- Senior Project/Program.cs
+++ Senior Project/Program.cs
@@ -2,39 +2,39 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Senior_Project.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);// Create a builder for the app
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();// For database errors
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
+    .AddEntityFrameworkStores<ApplicationDbContext>();// For Identity
+builder.Services.AddRazorPages();// For Razor Pages
 
-var app = builder.Build();
+var app = builder.Build();// Build the app
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+    app.UseMigrationsEndPoint();// For migrations
 }
 else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts();// For HTTPS
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseHttpsRedirection();// Redirects HTTP requests to HTTPS
+app.UseStaticFiles();// For the wwwroot folder
 
-app.UseRouting();
+app.UseRouting();// For routing requests to controllers
 
-app.UseAuthorization();
+app.UseAuthorization();// For authorization
 
-app.MapRazorPages();
+app.MapRazorPages();// For Razor Pages
 
-app.Run();
+app.Run();// For the default page
